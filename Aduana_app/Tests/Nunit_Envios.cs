62c6a8321759c5b298e098f80c6c43ac75aa2605
Fernando Rodriguez <fernandorz911@gmail.com>
2017-09-13 01:15:33 -0600

    pruebas unitarias importadora y envios

--- Aduana_app/Tests/Nunit_Envios.cs
+++ Aduana_app/Tests/Nunit_Envios.cs
@@ -10,6 +10,30 @@ namespace Aduana_app.Tests
     public class Nunit_Envios
     {
 
-        
+        [TestCase]
+        public void solicitar_catalogo_Vehiculos()
+        {
+            ws_Envios envios = new ws_Envios();
+            string salida = "{\"vehiculos\":[ {\"id_Vehiculo\": 300 , \"marca\": \"Toyota\", \"linea\": \"Yaris\", \"modelo\": \"2010\"}, {\"id_Vehiculo\": 310 , \"marca\": \"Nissan\", \"linea\": \"Navara\", \"modelo\": \"2016\"}, {\"id_Vehiculo\": 320 , \"marca\": \"Subaru\", \"linea\": \"WRX\", \"modelo\": \"2015\"}]}";
+            Assert.AreEqual(salida, envios.cargar_Vehiculos(55));
+        }
+
+        [TestCase]
+        public void calcular_Costo_viaje()
+        {
+            ws_Envios envios = new ws_Envios();
+            string salida = "{\"costo_Viaje\": 600}";
+            Assert.AreEqual(salida, envios.calcular_Costo_Viaje(55, 55.5));
+        }
+
+        [TestCase]
+        public void guardar_transferencia()
+        {
+            ws_Envios envios = new ws_Envios();
+            string salida = "{\"respuesta\": \"true\"}";
+            Assert.AreEqual(salida, envios.guardar_Transferencia(55, 55));
+        }
+
+
     }
 }
