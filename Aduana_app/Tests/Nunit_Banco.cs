103c8dfe71b82b3b08c1e84fa17d65a4853361c1
Ric25 <richar.2312@gmail.com>
2017-09-13 02:02:24 -0600

    Merge

---
+++ Aduana_app/Tests/Nunit_Banco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using Aduana_app.Web_Services;
+
+namespace Aduana_app.Tests
+{
+    [TestFixture]
+    public class Nunit_Banco
+    {
+        [TestCase]
+        public void transferencia_Cuenta()
+        {
+            ws_Banco objBanco = new ws_Banco();
+            string strResultado = objBanco.transferencia_Cuenta("909090", "919191", 2000);
+            Assert.AreEqual("{\"id_Transferencia\": \"30004999\",\"respuesta\": \"true\",\"detalle_Transferencia\": \"Transferencia Exitosa\"}", strResultado);
+        }
+    }
+}
---
+++ Aduana_app/Tests/Nunit_SAT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using Aduana_app.Web_Services;
+
+namespace Aduana_app.Tests
+{
+    [TestFixture]
+    public class Nunit_SAT
+    {
+        [TestCase]
+        public void calcular_Impuesto_Sat()
+        {
+            ws_Sat objSAT = new ws_Sat();
+            string strResultado = objSAT.calcular_Impuesto_Sat("Honda","Civic","2010");
+            Assert.AreEqual("{\"valor\": 400}", strResultado);
+        }
+
+        [TestCase]
+        public void registro_Id_Compra()
+        {
+            ws_Sat objSAT = new ws_Sat();
+            string strResultado = objSAT.registro_Id_Compra(1209990, 80000);
+            Assert.AreEqual("{\"respuesta\": \"true\"}", strResultado);
+        }
+
+        [TestCase]
+        public void guardar_Manifiesto()
+        {
+            ws_Sat objSAT = new ws_Sat();
+            string strResultado = objSAT.guardar_Manifiesto("Honda", "Civic", "2010", "20-10-2016", "EEUU");
+            Assert.AreEqual("{\"num_Manifiesto\": 0155}", strResultado);
+        }
+
+        [TestCase]
+        public void guardar_Declaracion()
+        {
+            ws_Sat objSAT = new ws_Sat();
+            string strResultado = objSAT.guardar_Declaracion("Honda", "Civic", "2010", 100000, "10-09-2017");
+            Assert.AreEqual("{\"num_Declaracion\": 0123}", strResultado);
+        }
+    }
+}
