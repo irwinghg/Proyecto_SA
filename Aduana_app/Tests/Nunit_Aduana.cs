cb9371563f8707e26f59936bca54b95acf42840f
Ric25 <richar.2312@gmail.com>
2017-09-13 02:02:24 -0600

    Merge Aduana

--- Aduana_app/Tests/Nunit_Aduana.cs
+++ Aduana_app/Tests/Nunit_Aduana.cs
@@ -10,9 +10,19 @@ namespace Aduana_app.Tests
     public class Nunit_Aduana
     {
         [TestCase]
-        public void calcular_costo_Aduana()
+        public void calcular_Costo_Aduana()
         {
-            
+            ws_Aduana objAduana = new ws_Aduana();
+            string strResultado = objAduana.calcular_Costo_Aduana("Honda", "Civic", "2010");
+            Assert.AreEqual("{\"costo_Aduana\": 135.57}", strResultado);
+        }
+
+        [TestCase]
+        public void guardar_Id_Transferencia()
+        {
+            ws_Aduana objAduana = new ws_Aduana();
+            string strResultado = objAduana.guardar_Id_Transferencia(1209990, 90000);
+            Assert.AreEqual("{\"respuesta\": \"true\"}", strResultado);
         }
     }
 }
